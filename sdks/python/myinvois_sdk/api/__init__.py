36ebb759807249316f706527c2ab8a37b874c0f9
zahidaramai <22869261+zahidaramai@users.noreply.github.com>
2026-01-13 11:18:23 +0000

    chore(sdks): regenerate SDKs from OpenAPI spec

---
+++ sdks/python/myinvois_sdk/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from myinvois_sdk.api.documents_api import DocumentsApi
+from myinvois_sdk.api.health_api import HealthApi
+from myinvois_sdk.api.sessions_api import SessionsApi
+from myinvois_sdk.api.submissions_api import SubmissionsApi
+from myinvois_sdk.api.taxpayer_api import TaxpayerApi
+
